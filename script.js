ae8ca14b92421272ba1bb643fd1bd3a8c9595fef
RyanFrazin <RyanFrazin@Gmail.com>
2022-09-27 23:00:24 -0700

    Ryan Frazin Sep 27, 2022 23:00
    Change background color of right side of form
    
    Add JavaScript to page
    
    Add alert to page  when password fields do not match
    
    Change dimensions of different fields on the page

---
+++ script.js
@@ -0,0 +1,11 @@
+const create = document.querySelector('#submitbutton');
+
+
+create.addEventListener('click', () => {
+    let pass = document.querySelector('#pass').value;
+    let passConfirm = document.querySelector('#pass2').value;
+    
+    if (passConfirm !== pass) {
+        alert('Password fields must match.')
+    }
+});
